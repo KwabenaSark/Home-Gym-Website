e018ca8f4b12a69772dc10922c4aae1a818a6234
Kwabena Sarkodieh <104778334+KwabenaSark@users.noreply.github.com>
2022-10-19 10:10:19 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'hge_database');
+define('DB_SERVER', ' sql8.freesqldatabase.com');
+define('DB_USERNAME', 'sql8527701');
+define('DB_PASSWORD', 'uctBWU35RY');
+define('DB_NAME', 'sql8527701');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
